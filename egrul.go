7cec944363f1b87a5d6ba741476d93c6327979ea
Novikov Roman <service.for.roman@gmail.com>
2022-07-04 16:20:51 +0300

    add parser egrul

---
+++ egrul.go
@@ -0,0 +1,90 @@
+package gofns
+
+import (
+	"encoding/json"
+	"errors"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+const egrulUrl = "https://egrul.nalog.ru"
+
+const (
+	LegalEntity      = "ul"
+	IndividualEntity = "fl"
+)
+
+type Egrul struct {
+	Company         string    `json:"c"`
+	General         string    `json:"g,omitempty"`
+	Address         string    `json:"a,omitempty"`
+	Inn             string    `json:"i,omitempty"`
+	Name            string    `json:"n"`
+	Ogrn            string    `json:"o"`
+	Kpp             string    `json:"p"`
+	RegistrationRaw string    `json:"r"`
+	Registration    time.Time `json:"-"`
+	Token           string    `json:"t"`
+	Entity          string    `json:"k"`
+}
+
+//EgrulByInn получение сведений о юридическом лице по ИНН
+func (c *Client) EgrulByInn(inn string) (egruls []*Egrul, err error) {
+	headers := map[string]string{
+		"User-Agent":       userAgent,
+		"Referer":          egrulUrl,
+		"Cache-Control":    "no-cache",
+		"Pragma":           "no-cache",
+		"X-Requested-With": "XMLHttpRequest",
+		"Accept-Encoding":  "gzip, deflate, br",
+	}
+
+	data := &url.Values{
+		"vyp3CaptchaToken":          {""},
+		"page":                      {""},
+		"query":                     {inn},
+		"region":                    {""},
+		"PreventChromeAutocomplete": {""},
+	}
+
+	var b []byte
+	if b, err = c.post(egrulUrl, data, &headers); err != nil {
+		err = newErrBadResponse(err.Error())
+		return
+	}
+
+	var respToken struct {
+		T               string `json:"t"`
+		CaptchaRequired bool   `json:"captchaRequired"`
+	}
+
+	if err = json.Unmarshal(b, &respToken); err != nil {
+		return
+	}
+
+	if respToken.CaptchaRequired {
+		err = errors.New("Требует капчу. " + string(b))
+		return
+	}
+
+	timestamp := strconv.Itoa(int(time.Now().Unix()))
+	q := "?r=" + timestamp + "&_=" + timestamp
+	if b, err = c.get(egrulUrl+"/search-result/"+respToken.T+"/"+q, &headers); err != nil {
+		err = newErrBadResponse(err.Error())
+		return
+	}
+
+	var rows struct {
+		Rows []*Egrul
+	}
+	if err = json.Unmarshal(b, &rows); err != nil {
+		return
+	}
+
+	egruls = rows.Rows
+	for i := range egruls {
+		egruls[i].Registration, _ = time.Parse(LayoutDate, egruls[i].RegistrationRaw)
+	}
+	return
+}
