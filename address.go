041726924910d5f1bf1bc318cf83b31ca31be045
Novikov Roman <service.for.roman@gmail.com>
2023-03-17 16:57:49 +0300

    fix parse address

--- address.go
+++ address.go
@@ -105,7 +105,7 @@ func (a *Address) parse() (err error) {
 	// Н. Новгород - Нижний Новгород
 	res = regexp.MustCompile(`(?si)Н\.\s*Новгород`).ReplaceAllString(res, "Нижний Новгород")
 
-	res = regexp.MustCompile(`(?si)(\sр\s*\.\s*п\s*\.|\sрп\.*|п\s*\.\s*г\s*\.\s*т\s*\.|хут.|пгт\.*|пос\.|[^а-яё]сп\.*[^а-яё]|[^а-яё]им.[^а-яё])`).ReplaceAllString(res, "")
+	res = regexp.MustCompile(`(?si)(\sр\s*\.\s*п\s*\.|\sрп\.*|п\s*\.\s*г\s*\.\s*т\s*\.|хут\.|пгт\.*|пос\.|[^а-яё]сп\.*[^а-яё]|[^а-яё]им\.[^а-яё])`).ReplaceAllString(res, "")
 	res = regexp.MustCompile(`(?si)(\sп\.|\sс\.|рц\.|\sд\.)`).ReplaceAllString(res, " ")
 
 	// мкр
