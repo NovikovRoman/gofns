dd4ba51f2af38dae919b75ea184daed9c1b922dc
Novikov Roman <service.for.roman@gmail.com>
2023-09-26 08:48:35 +0300

    refact search_inn_test

--- search_inn_test.go
+++ search_inn_test.go
@@ -14,28 +14,31 @@ func TestClient_SearchInn(t *testing.T) {
 	ctx := context.Background()
 
 	var (
+		birthday time.Time
+		inn      string
 		passport Document
 		err      error
 	)
-	passport, err = NewDocument(os.Getenv("PERSON_PASSPORT"), DocumentPassportRussia, nil)
-	require.Nil(t, err)
 
-	var birthday time.Time
-	birthday, err = time.Parse(LayoutDate, os.Getenv("PERSON_BIRTHDAY"))
-	require.Nil(t, err)
+	if os.Getenv("PERSON_PASSPORT") != "" {
+		passport, err = NewDocument(os.Getenv("PERSON_PASSPORT"), DocumentPassportRussia, nil)
+		require.Nil(t, err)
 
-	owner := &Person{
-		LastName:   os.Getenv("PERSON_LASTNAME"),
-		Name:       os.Getenv("PERSON_NAME"),
-		SecondName: os.Getenv("PERSON_SECONDNAME"),
-		Birthday:   birthday,
-		Document:   passport,
+		birthday, err = time.Parse(LayoutDate, os.Getenv("PERSON_BIRTHDAY"))
+		require.Nil(t, err)
+
+		owner := &Person{
+			LastName:   os.Getenv("PERSON_LASTNAME"),
+			Name:       os.Getenv("PERSON_NAME"),
+			SecondName: os.Getenv("PERSON_SECONDNAME"),
+			Birthday:   birthday,
+			Document:   passport,
+		}
+
+		inn, err = c.SearchInn(ctx, owner)
+		require.Nil(t, err)
+		require.Equal(t, inn, os.Getenv("PERSON_INN"))
 	}
-
-	var inn string
-	inn, err = c.SearchInn(ctx, owner)
-	require.Nil(t, err)
-	require.Equal(t, inn, os.Getenv("PERSON_INN"))
 
 	passport, err = NewDocument("6767 123456", DocumentPassportRussia, nil)
 	require.Nil(t, err)
@@ -54,29 +57,4 @@ func TestClient_SearchInn(t *testing.T) {
 	inn, err = c.SearchInn(ctx, person)
 	require.Nil(t, err)
 	require.Equal(t, inn, "")
-}
-
-func TestClient_SearchInn2(t *testing.T) {
-	c := NewClient(nil)
-	ctx := context.Background()
-
-	var (
-		passport Document
-		err      error
-	)
-	passport, err = NewDocument("9702 736056", DocumentPassportRussia, nil)
-	require.Nil(t, err)
-
-	person := &Person{
-		LastName:   "Новиков",
-		Name:       "Роман",
-		SecondName: "Владимирович",
-		Birthday:   time.Date(1978, 5, 4, 0, 0, 0, 0, time.UTC),
-		Document:   passport,
-	}
-
-	var inn string
-	inn, err = c.SearchInn(ctx, person)
-	require.Nil(t, err)
-	require.Equal(t, inn, "212404684217")
 }
